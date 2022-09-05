79db3868e17cc36063c45dcd56fde0fe6c0afa29
hafizmaulanaazhar <77614062+hafizmaulanaazhar@users.noreply.github.com>
2022-09-05 22:43:54 +0700

    Second Commit

---
+++ Mission2_VanillaJS/assets/index.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+    //variabel
+    $surface =$('.surface');
+    $car = $('car');
+    $img = $('car img');
+    let flag = true;
+
+    const cars = ['./assets/Img_05.png' , './assets/Img_06.png']
+    //keypress
+    $(document).on('keypress', function(e){
+        if(e.which == 13){
+            $(surface).toggleClass('moveRight');
+            $(car).toggleClass('suspension');
+        }
+    })
+
+    //light
+    $(document).on('keypress', function(e){
+        if(e.which == 119){
+           if(flag){
+            flag = false;
+            img.attr('src',cars[0]);
+           }else{
+            flag = true;
+            img.attr('src',cars[1]);
+           }
+        }
+    })
+});
